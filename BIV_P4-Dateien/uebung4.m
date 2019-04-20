d8f256fc8cb7fbe4a406dfede50eefe476170bbc
chefkoch24 <kuennecke-felix@web.de>
2019-04-20 15:13:42 +0200

    aufagbe 4 angefangen

---
+++ BIV_P4-Dateien/uebung4.m
@@ -0,0 +1,26 @@
+shadingimage = double(imread('shading.jpg'))/255;
+shadingimage = shadingimage(:,:,1);
+imshow(shadingimage);
+%% 1a)
+mask = [1 2 1];
+ergebnis = conv2(shadingimage, mask);
+figure;
+imshow(ergebnis);
+% Warum ist das Ergebnis heller?
+% weil nicht durch die Summe aller Werte geteilt wird
+newmask = [1 2 1] ./5;
+ergebnis = conv2(shadingimage, newmask);
+figure;
+imshow(ergebnis);
+%% 1b)
+ergebnis = shadingimage;
+figure;
+imshow(ergebnis);
+for m = 1:50
+ergebnis = conv2(shadingimage, newmask);
+end;
+figure;
+imshow(ergebnis);
+% a) das bild ist komplett schwarz?
+% b) das Bild bestand vorher aus 218x303 Pixel und besteht danach aus 218x403 Pixeln
+% c) 
